03c6519cadfea6ed671581f1101fc85cb43c0c65
xreotrace <marufbillah.dev@gmail.com>
2025-04-13 01:36:10 +0600

    Update code

--- bot/core/strategies/flash_loan.js
+++
@@ -1,8 +0,0 @@
-const { BaseStrategy } = require("./base");
-const { Connection } = require("@solana/web3.js");
-
-class FlashLoanStrategy extends BaseStrategy {
-  async execute(trade) {
-    // Flash loan specific logic
-  }
-}
